af7336c3d42247ced50af8fd3a9f7ef3f25123b3
Trim Bresilla <trim.bresilla@gmail.com>
2025-02-23 17:21:02 +0100

    forced reinit

--- clean_up.sh
+++
@@ -1,8 +0,0 @@
-echo Delete Build Binaries Saved Intermediate
-rm -r Build Binaries Saved Intermediate DerivedDataCache
-
-echo Delete Binaries Intermediate in Plugin
-for d in Plugins/* ; do
-    rm -r $d/Binaries $d/Intermediate
-done
-
--- get_deps.sh
+++
@@ -1,7 +0,0 @@
-sudo add-apt-repository ppa:dotnet/backports
-
-sudo apt update
-sudo apt upgrade
-
-
-sudo apt-get install -y dotnet-runtime-6.0 rsync just direnv libspdlog*
